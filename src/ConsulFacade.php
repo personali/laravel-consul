fe676e114c38ef70c4b89c678a165b724ad2a9ef
amirbilu <amirbilu@gmail.com>
2016-06-15 14:31:23 +0300

    fix typo

--- src/ConsulFacade.php
+++ src/ConsulFacade.php
@@ -1,20 +1,11 @@
 <?php
 namespace Personali\LaravelConsul;
 use Illuminate\Support\Facades\Facade;
-/**
- * Class AnalyticsFacade
- *
- * @package Ipunkt\LaravelAnalytics
- */
-class AnalyticsFacade extends Facade
-{
-    /**
-     * facade accessor
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor()
-    {
+
+
+class ConsulFacade extends Facade{
+
+    protected static function getFacadeAccessor(){
         return 'consul';
     }
 }
